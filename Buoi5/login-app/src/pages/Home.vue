26bbdddd37d5871ddec374a2c8aa51c3ef7c52ff
trantam291102 <97502878+trantam291102@users.noreply.github.com>
2023-09-24 23:03:18 +0700

    Update Home.vue

--- Buoi5/login-app/src/pages/Home.vue
+++ Buoi5/login-app/src/pages/Home.vue
@@ -1,15 +1,22 @@
 <script>
+//import GetData from "../utils/api.js";
 export default {
-  name: "Home",
-  beforeMount() {
-    let isLogin = false;
-    if (!isLogin) {
-      this.$router.push("/login");
-    }
+  name: "App",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      if (this.username && this.password) {
+        console.log(this.username, this.password);
+        alert(this.username, this.password);
+        return;
+      }
+      alert("Vui long nhap tai khoan va mat khau");
+    },
   },
 };
 </script>
-
-<template><h1>Xin chao Hoc Tran</h1></template>
-
-<style></style>
